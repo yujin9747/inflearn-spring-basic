3e4dfab53cc52d6a34ea73f42578002d4bec156f
jang-yujin <jyj9747@naver.com>
2023-07-03 21:48:28 +0900

    test

--- src/main/java/hello/core/member/service/MemberServiceImpl.java
+++ src/main/java/hello/core/member/service/MemberServiceImpl.java
@@ -19,5 +19,7 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
+
+
     }
 }
