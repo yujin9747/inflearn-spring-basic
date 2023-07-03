fbf777a9277ec1dee2815ff242e557590dad4733
jang-yujin <jyj9747@naver.com>
2023-07-03 21:33:00 +0900

    spotless 자동화 테스트

--- src/main/java/hello/core/member/service/MemberServiceImpl.java
+++ src/main/java/hello/core/member/service/MemberServiceImpl.java
@@ -4,7 +4,9 @@ import hello.core.member.domain.Member;
 import hello.core.member.repository.MemberRepository;
 import hello.core.member.repository.MemoryMemberRepository;
 
-
+/**
+ *
+ */
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -17,7 +19,5 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
-
-
     }
 }
