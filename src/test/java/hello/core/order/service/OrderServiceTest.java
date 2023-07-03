1fef089ddf0c1158e45616b3db3e209b41938c7f
jang-yujin <jyj9747@naver.com>
2023-07-03 23:24:23 +0900

    :test_tube: Order service test - createOrder
    SUCCESS

--- src/test/java/hello/core/order/service/OrderServiceTest.java
+++ src/test/java/hello/core/order/service/OrderServiceTest.java
@@ -1,4 +1,24 @@
-import static org.junit.jupiter.api.Assertions.*;
+package hello.core.order.service;
+
+import hello.core.member.domain.Grade;
+import hello.core.member.domain.Member;
+import hello.core.member.service.MemberService;
+import hello.core.member.service.MemberServiceImpl;
+import hello.core.order.domain.Order;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class OrderServiceTest {
-  
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
 }
