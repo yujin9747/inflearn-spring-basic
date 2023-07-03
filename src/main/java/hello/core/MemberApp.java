05d49ba49801bbcc1cd8734ff55b1370a52e1bba
jang-yujin <jyj9747@naver.com>
2023-07-03 11:59:46 +0900

    basic code - OCP, DIP 위반

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,18 @@
+package hello.core;
+
+import hello.core.member.domain.Grade;
+import hello.core.member.domain.Member;
+import hello.core.member.service.MemberService;
+import hello.core.member.service.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("find member = " + findMember.getName());
+        System.out.println("new member = " + member.getName());
+    }
+}
---
+++ src/main/java/hello/core/member/service/MemberServiceImpl.java
@@ -0,0 +1,19 @@
+package hello.core.member.service;
+
+import hello.core.member.repository.MemberRepository;
+import hello.core.member.repository.MemoryMemberRepository;
+import hello.core.member.domain.Member;
+
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
