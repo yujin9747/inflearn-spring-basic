9c1447dbefaa28a16abcc6aff79cabcfd927579a
jang-yujin <jyj9747@naver.com>
2023-07-04 12:22:10 +0900

    :sparkles: Spring container를 사용해서 빈 등록
    ApplicationContext - spring container

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,24 +8,32 @@ import hello.core.member.service.MemberService;
 import hello.core.member.service.MemberServiceImpl;
 import hello.core.order.service.OrderService;
 import hello.core.order.service.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 구성 영역
+@Configuration
 public class AppConfig {
+    // @Bean : spring container에 빈을 등록한다
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); // 생성자 주입
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     // 역할을 모두 드러내는 방식으로 메소드를 뽑아낸다
-    private DiscountPolicy discountPolicy() {
-//        return new FixDiscountPolicy();
-        return new RateDiscountPolicy();    // 구성영역에서만 코드를 수정하면 된다 -> 클라이언트 코드 수정 필요 없음.
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        //        return new FixDiscountPolicy();
+        return new RateDiscountPolicy(); // 구성영역에서만 코드를 수정하면 된다 -> 클라이언트 코드 수정 필요 없음.
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,11 +3,18 @@ package hello.core;
 import hello.core.member.domain.Grade;
 import hello.core.member.domain.Member;
 import hello.core.member.service.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        //기존에는 spring을 사용하지 않고 AppConfig 에서 직접 꺼내서 사용
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+        // AppConfig에 있는 환경 설정 정보를 가지고 spring container에 빈을 생성해서 관리한다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // spring container에서 빈을 가져오는 방식으로 변경
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
