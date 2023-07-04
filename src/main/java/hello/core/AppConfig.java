c7e036274caadde64f6e092342422fd4baf2142c
jang-yujin <jyj9747@naver.com>
2023-07-04 11:12:53 +0900

    :sparkles: DiscountPolicy 구현체 교체
    AppConfig 구성영역 코드만 수정

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,7 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.repository.MemberRepository;
 import hello.core.member.repository.MemoryMemberRepository;
 import hello.core.member.service.MemberService;
@@ -9,6 +9,7 @@ import hello.core.member.service.MemberServiceImpl;
 import hello.core.order.service.OrderService;
 import hello.core.order.service.OrderServiceImpl;
 
+// 구성 영역
 public class AppConfig {
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); // 생성자 주입
@@ -18,9 +19,10 @@ public class AppConfig {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    //역할을 모두 드러내는 방식으로 메소드를 뽑아낸다
+    // 역할을 모두 드러내는 방식으로 메소드를 뽑아낸다
     private DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();    // 구성영역에서만 코드를 수정하면 된다 -> 클라이언트 코드 수정 필요 없음.
     }
 
     private MemberRepository memberRepository() {
