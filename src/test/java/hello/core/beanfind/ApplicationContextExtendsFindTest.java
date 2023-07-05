d61b3eb126df7cc3f33c2a1768f8e261213dffa3
jang-yujin <jyj9747@naver.com>
2023-07-05 17:04:50 +0900

    :sparkles: Xml로 configuration 파일 생성하기.
    java, xml 그리고 한 가지더 해서 총 3가지 방식 적용 가능

--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -1,8 +1,11 @@
 package hello.core.beanfind;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
@@ -10,17 +13,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Map;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class ApplicationContextExtendsFindTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
 
     @Test
     @DisplayName("부모 타입으로 조회 시, 자식이 둘 이상 있으면 중복 오류가 발생한다")
     void findBeanByParentTypeDuplicate() {
-        org.junit.jupiter.api.Assertions.assertThrows(NoUniqueBeanDefinitionException.class, () -> ac.getBean(DiscountPolicy.class));
+        org.junit.jupiter.api.Assertions.assertThrows(
+                NoUniqueBeanDefinitionException.class, () -> ac.getBean(DiscountPolicy.class));
     }
 
     @Test
@@ -32,7 +32,7 @@ public class ApplicationContextExtendsFindTest {
 
     @Test
     @DisplayName("특정 하위 타입으로 조회한다")
-        // 좋은 방법은 아니다.
+    // 좋은 방법은 아니다.
     void findBeanBySubType() {
         RateDiscountPolicy bean = ac.getBean(RateDiscountPolicy.class);
         assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
