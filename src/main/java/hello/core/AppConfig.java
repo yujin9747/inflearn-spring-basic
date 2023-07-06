a2365f4a7cd49dc65fa1e07ca22d8ecb5d7f2bce
jang-yujin <jyj9747@naver.com>
2023-07-06 10:44:22 +0900

    :sparkles: @Configuration으로 생성된 CGLIB 클래스 동작 방식 확인
    .

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -13,17 +13,20 @@ import org.springframework.context.annotation.Configuration;
 
 // 구성 영역
 // memberRepository() 가 여러번 호출되면서 singleton이 깨지는 것 처럼 보이지만, @Configuration을 붙이면 싱글톤을 보장해준다.
-@Configuration
+@Configuration // @Configuration을 붙이지 않으면 스프링 컨테이너에는 잘 등록이 되나, 싱글톤이 깨진다(CGLIB 자식 클래스를 사용하지 않기 때문).
 public class AppConfig { // 자바를 이용하는건 factoryMethod를 통해서 등록하는 방식, xml은 해당하지 않는다.
     // @Bean : spring container에 빈을 등록한다
 
-    // 자바 코드 그대로라면 memberRepository는 총 3번 출력되어야 한다.
+    // 자바 코드 그대로라면 memberRepository는 총 3번 출력되어야 한다. -> @Configuration을 붙이지 않아서 CGLIB를 사용하지 않는 경우 memberRepository가 총 3번 생성된다.(싱글톤 깨짐)
     // AppConfig.memberService
     // AppConfig.memberRepository
     // AppConfig.memberRepository
     // AppConfig.orderService
     // AppConfig.memberRepository
     // 하지만 각각 한 번씩만 출력된다. -> @Configuration을 붙이면 싱글톤을 보장해준다.
+
+    // @Autowired를 통해 싱글톤을 유지할 수도 있다.
+//    @Autowired MemberRepository memberRepository; // 생성자 주입
     @Bean
     public MemberService memberService() {
         System.out.println("AppConfig.memberService");
--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -29,4 +29,26 @@ public class ConfigurationSingletonTest {
         Assertions.assertThat(memberRepository1).isSameAs(memberRepository);
         Assertions.assertThat(memberRepository2).isSameAs(memberRepository);
     }
+
+    @Test
+    void configurationDeep() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        // bean.getClass() = class hello.core.AppConfig$$SpringCGLIB$$0
+        // 스프링이 AppConfig를 상속받는 AppConfig@CGLIB 라는 클래스를 만들고, 그 클래스를 스프링 빈으로 등록한다.
+        System.out.println("bean.getClass() = " + bean.getClass());
+        // @Configuration을 빼면 bean.getClass() = class hello.core.AppConfig 이 출력된다.
+        // CGLIB 예시 코드
+//        @Bean
+//        public MemberRepository memberRepository(){
+//            if(memberRepository가 이미 스프링 컨테이너에 있으면?){
+//
+//            }
+//            else { // 스프링 컨테이너에 없으면?
+//                기존 로직(AppConfig.class 내의 memberRepository())을 호출해서 MemoryMemberRepository를 생성하고 스프링 빈에 등록한다.
+//                return 반환.
+//            }
+//        }
+    }
 }
